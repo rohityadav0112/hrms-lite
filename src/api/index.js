935e5644c63ac914a43b645615789b4e6dff1b10
rohityadav0112 <0112cs221112@gmail.com>
2026-02-25 16:34:19 +0530

    sample

--- src/api/index.js
+++ src/api/index.js
@@ -12,3 +12,4 @@ export const getDashboard = () => api.get('/api/dashboard')
 
 export const getAttendance = (params) => api.get('/api/attendance', { params })
 export const markAttendance = (data) => api.post('/api/attendance', data)
+console.log("Hello")
