3136d1494035e1348b9e557680bb723d2ca86050
rohityadav0112 <0112cs221112@gmail.com>
2026-02-25 17:31:27 +0530

    update url

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,11 @@ const api = axios.create({
   baseURL: import.meta.env.REACT_APP_VITE_API_URL || 'https://hrms-lite-production-bc0b.up.railway.app',
 })
 
-export const getEmployees = () => api.get('/api/employees')
-export const createEmployee = (data) => api.post('/api/employees', data)
+export const getEmployees = () => api.get('/api/employees/')
+export const createEmployee = (data) => api.post('/api/employees/', data)
 export const deleteEmployee = (id) => api.delete(`/api/employees/${id}`)
 
 export const getDashboard = () => api.get('/api/dashboard')
 
-export const getAttendance = (params) => api.get('/api/attendance', { params })
-export const markAttendance = (data) => api.post('/api/attendance', data)
+export const getAttendance = (params) => api.get('/api/attendance/', { params })
+export const markAttendance = (data) => api.post('/api/attendance/', data)
