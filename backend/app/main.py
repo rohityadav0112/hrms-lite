02147bb809c1919d6df52559556c47513f0b6570
Rohit Yadav <0112CS221112@gmail.com>
2026-02-25 17:16:58 +0530

    Update CORS allowed origins in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://hrms-lite-production-bc0b.up.railway.app"
+        "https://hrms-lite-eight-gold.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
